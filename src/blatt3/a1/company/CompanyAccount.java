b9c8947cad54f82162e192eccacacd74d4f38b4e
ebroll <ebroll@smail.uni-koeln.de>
2025-04-29 22:49:37 +0200

    HA3

--- src/blatt3/a1/company/CompanyAccount.java
+++ src/blatt3/a1/company/CompanyAccount.java
@@ -1,6 +1,7 @@
 package blatt3.a1.company;
 
-import a1.Acc.Account;
+
+import blatt3.a1.Acc.Account;
 
 public class CompanyAccount extends Account {
     public CompanyAccount(String owner, double balance, int pin, String internalNote) {
